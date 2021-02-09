f1a21677db42f457f2e27974da2a21b6e75d6ff1
Alan Ballanger <alan.ballanger@live.fr>
2021-02-09 15:30:30 +0100

    second commit

--- src/views/Appli.vue
+++ src/views/Appli.vue
@@ -4,11 +4,22 @@
 
 <script>
 import useSound from 'vue-use-sound'
-import buttonSfx from '../assets/perfect.mp3'
+import perfectSfx from '../assets/perfect.mp3'
+import percussionSfx from '../assets/percussion.mp3'
+import pianoSfx from '../assets/piano.mp3'
+import timbalesSfx from '../assets/timbales.mp3'
+import bassSfx from '../assets/bass.mp3'
+import bass2Sfx from '../assets/bass2.mp3'
 
 export default {
   setup() {
-    const [play] = useSound(buttonSfx)
+    const [play] = useSound(perfectSfx)
+    const [play] = useSound(percussionSfx)
+    const [play] = useSound(pianoSfx)
+    const [play] = useSound(perfectSfx)
+    const [play] = useSound(perfectSfx)
+    const [play] = useSound(perfectSfx)
+    const [play] = useSound(perfectSfx)
 
     return {
       play,

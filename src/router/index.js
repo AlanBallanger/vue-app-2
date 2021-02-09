b9f2c504f336e1a92ee945a769c6f8b7fd089ef7
Alan Ballanger <alan.ballanger@live.fr>
2021-02-09 15:21:48 +0100

    First commit

--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/appli',
+    name: 'Appli',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Appli.vue')
   }
 ]
 
---
+++ src/views/Appli.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="play">Play a sound</button>
+</template>
+
+<script>
+import useSound from 'vue-use-sound'
+import buttonSfx from '../assets/perfect.mp3'
+
+export default {
+  setup() {
+    const [play] = useSound(buttonSfx)
+
+    return {
+      play,
+    }
+  },
+}
+</script>

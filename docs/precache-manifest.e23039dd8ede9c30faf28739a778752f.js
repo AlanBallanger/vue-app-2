864dd8a3f610096f1df2d497d6bdce8b9c2f364c
Alan Ballanger <alan.ballanger@live.fr>
2021-02-09 14:28:30 +0100

    first commit

---
+++ docs/precache-manifest.e23039dd8ede9c30faf28739a778752f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13cd45d035b6254e0e18",
+    "url": "/vue-app-2/css/app.ddeedaa8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-app-2/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "f6a6521c43c3f9c5d314b6f1f3723853",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "0c69ad25f42d2e74280d",
+    "url": "/vue-app-2/js/about.592c0bde.js"
+  },
+  {
+    "revision": "13cd45d035b6254e0e18",
+    "url": "/vue-app-2/js/app.09115884.js"
+  },
+  {
+    "revision": "ba2a6fda9ddaaddeab4b",
+    "url": "/vue-app-2/js/chunk-vendors.f449932c.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
